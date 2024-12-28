3bb9dab9faa98421cca5f2ad0a7534d485b09ce0
NAVEEN KUMAR <nk946432@gmail.com>
2024-12-28 22:59:54 +0530

    almost fetched data

--- movie-app/src/components/Card.jsx
+++ movie-app/src/components/Card.jsx
@@ -1,18 +1,23 @@
 import backup from"../assets/react.svg"
 import { Link } from "react-router-dom"
- export const Card = () => {
+ export const Card = ({movie}) => {
+  const {poster_path,id,overview,release_date,title,vote_average,vote_count}=movie
+
+  const image=poster_path ? `https://image.tmdb.org/t/p/original${poster_path}`: backup;
+
   return (
     <div className="col">
       <div className="card shadow-sm">
-      <img src={backup} alt="" className="card-img-top" />
+      <img src={image} alt="" className="card-img-top" />
         <div className="card-body">
-         <h5 className="card-title text-primary">Movie Title</h5>
-         <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum optio, cupiditate laborum sapiente illo eligendi.</p>
+         <h5 className="card-title text-primary">{title}</h5>
+         <p className="card-text text-overflow-2">{overview}</p>
+         <p>Release On {release_date}</p>
          <div className="d-flex justify-content-between align-items-center">
-          <Link to='' className="btn btn-sm btn-outline-primary">Read More</Link>
+          <Link to='' className="btn btn-sm btn-outline-primary stretched-link">Read More</Link>
           <small>
             <i className="bi bi-star-fill text-warning"> </i>
-            7.25 | 250
+            {vote_average} | {vote_count} reviews
           </small>
          </div>
         </div>
--- movie-app/src/routes/AllRouts.jsx
+++ movie-app/src/routes/AllRouts.jsx
@@ -5,10 +5,10 @@ const AllRouts = () => {
   return (
     <>
         <Routes>
-            <Route path="/" element={<MovieList title="your guide to Great Movies"/>}/>
-            <Route path="movies/popular" element={<MovieList title="Popular Movies"/>}/>
-            <Route path="movies/top" element={<MovieList title="Top Rated Movies"/>}/>
-            <Route path="movies/upcomming" element={<MovieList title="Upcoming Movies"/>}/>
+            <Route path="/" element={<MovieList title="your guide to Great Movies"apiPath="movie/now_playing"/>}/>
+            <Route path="movies/popular" element={<MovieList title="Popular Movies" apiPath="movie/popular"/>}/>
+            <Route path="movies/top" element={<MovieList title="Top Rated Movies" apiPath="movie/top_rated"/>}/>
+            <Route path="movies/upcomming" element={<MovieList title="Upcoming Movies" apiPath="movie/upcoming"/>}/>
         </Routes>
     
     </>
